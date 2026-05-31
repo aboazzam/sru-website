aae28216dd9d95b6473a0baa1b2119c731969ed1
aboazzam <malmohaimeed@gmail.com>
2026-05-31 19:55:30 +0300

    Create admin.js

---
+++ admin/admin.js
@@ -0,0 +1,14 @@
+async function saveNews(item) {
+  const res = await fetch('data/save-news.php', { // بدون ../ لأننا داخل admin/
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(item)
+  });
+
+  const result = await res.json();
+  if (result.status === 'ok') {
+    alert('تم حفظ الخبر بنجاح');
+  } else {
+    alert('حدث خطأ أثناء الحفظ');
+  }
+}
